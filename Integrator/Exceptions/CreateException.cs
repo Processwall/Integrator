d4f23df1da9874786f770e19ecd9d31eaa619477
Richard Mason <richard.mason@aessis.com>
2017-08-09 09:46:04 +0100

    Moved ParameterException to Integrator

---
+++ Integrator/Exceptions/CreateException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Integrator.Exceptions
+{
+    public class CreateException : Exception
+    {
+        public CreateException(String Message)
+            : base(Message)
+        {
+        }
+    }
+}
--- Integrator/Exceptions/ParameterException.cs
+++ Integrator/Exceptions/ParameterException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Integrator.Connection.Exceptions
+namespace Integrator.Exceptions
 {
     public class ParameterException : Exception
     {

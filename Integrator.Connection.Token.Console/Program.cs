b600ff036ddc92af54bc923182fbbf683d36bacc
Richard Mason <richard.mason@aessis.com>
2017-09-11 15:51:40 +0100

    Added utility to create Connection tokens

---
+++ Integrator.Connection.Token.Console/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Integrator.Connection.Token.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Utility to create a Token for a Connection");
+            System.Console.WriteLine();
+
+            // Get Filename of DLL
+            System.Console.Write("Enter DLL Filename: ");
+            FileInfo filename = new FileInfo(System.Console.ReadLine());
+            
+            // Get Name of Connection
+            System.Console.Write("Enter Connection Name: ");
+            String name = System.Console.ReadLine();
+            
+            // Build Session
+            Session session = new Session(name, filename);
+
+            // Select Connection Type
+            System.Console.WriteLine();
+            
+            int a = 1;
+
+            foreach(Type type in session.ConnectionTypes)
+            {
+                System.Console.WriteLine(a.ToString() + ". " + type.FullName);
+            }
+
+            System.Console.WriteLine();
+            System.Console.Write("Select Connection Type: ");
+            String typenumberstring = System.Console.ReadLine();
+            Int32 typenumber = -1;
+            Type connectiontype = null;
+
+            if (Int32.TryParse(typenumberstring, out typenumber))
+            {
+                typenumber--;
+
+                if ((typenumber >= 0) && (typenumber < session.ConnectionTypes.Count()))
+                {
+                    connectiontype = session.ConnectionTypes.ElementAt(typenumber);
+                }
+            }
+
+            if (connectiontype != null)
+            {
+                Connection.ISession connection = (Connection.ISession)System.Activator.CreateInstance(connectiontype);
+                connection.Name = name;
+
+                System.Console.WriteLine();
+                System.Console.WriteLine("Enter Parameter Values: ");
+
+                foreach (Integrator.Parameter parameter in connection.Parameters)
+                {
+                    System.Console.Write(" - " + parameter.Name + ": ");
+                    parameter.Value = System.Console.ReadLine();
+                }
+
+                System.Console.WriteLine();
+                System.Console.WriteLine(connection.Parameters.Token());
+            }
+            else
+            {
+                System.Console.WriteLine("Connection Type not selected");
+            }
+
+            System.Console.WriteLine();
+            System.Console.Write("Any key to exit: ");
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ Integrator.Connection.Token/Session.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace Integrator.Connection.Token
+{
+    public class Session
+    {
+        public String Name { get; private set; }
+
+        public FileInfo Filename { get; private set; }
+
+        private Assembly _assembly;
+        private Assembly Assembly
+        {
+            get
+            {
+                if (this._assembly == null)
+                {
+                    this._assembly = Assembly.LoadFrom(this.Filename.FullName);
+                }
+
+                return this._assembly;
+            }
+        }
+
+        private List<Type> _connectionTypes;
+        public IEnumerable<Type> ConnectionTypes
+        {
+            get
+            {
+                if (this._connectionTypes == null)
+                {
+                    this._connectionTypes = new List<Type>();
+
+                    foreach(Type type in this.Assembly.GetTypes())
+                    {
+                        if (type.GetInterfaces().Contains(typeof(Connection.ISession)))
+                        {
+                            this._connectionTypes.Add(type);
+                        }
+                    }
+                }
+
+                return this._connectionTypes;
+            }
+        }
+
+        public Session(String Name, FileInfo Filename)
+        {
+            this.Name = Name;
+            this.Filename = Filename;
+        }
+    }
+}
